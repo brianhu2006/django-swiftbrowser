a6a31aac032a6a846a3f915298766244c556a8a6
Bing Hu <hubingsh@cn.ibm.com>
2017-06-02 13:19:41 +0800

    pin django version to 1.6.11

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url='https://github.com/cschwede/django-swiftbrowser',
     author='Christian Schwede',
     author_email='info@cschwede.de',
-    install_requires=['django>=1.5', 'python-swiftclient'],
+    install_requires=['django==1.6.11', 'python-swiftclient'],
     zip_safe=False,
     classifiers=[
         'Environment :: Web Environment',
